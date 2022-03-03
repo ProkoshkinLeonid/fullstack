1e6594eeac6587e96302977b035c5fb1e07b2c80
Прокошкин Леонид <55481696+forjke777@users.noreply.github.com>
2022-03-03 11:47:20 +0300

    start new arh

--- server/Routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-
-const router = new Router()
-const authController = require('../Controllers/auth.controller')
-
-router.post('/login', authController.login)
-
-module.exports = router
